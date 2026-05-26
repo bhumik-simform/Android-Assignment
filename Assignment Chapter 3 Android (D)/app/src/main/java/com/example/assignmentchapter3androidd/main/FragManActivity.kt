610923b5f4680671665b28841abbbb10cae4606b
Bhumik Poshiya <bhumik.poshiya@simformsolutions.com>
2026-05-26 12:23:18 +0530

    TE-T534: Restructure project for implementation of jetpack navigation and fragment manager in same module app

--- Assignment Chapter 3 Android (D)/app/src/main/java/com/example/assignmentchapter3androidd/main/FragManActivity.kt
+++ Assignment Chapter 3 Android (D)/app/src/main/java/com/example/assignmentchapter3androidd/main/FragManActivity.kt
@@ -1,31 +1,25 @@
-package com.example.assignmentchapter3androidd
+package com.example.assignmentchapter3androidd.main
 
 import android.os.Bundle
-import androidx.activity.OnBackPressedDispatcher
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
+import com.example.assignmentchapter3androidd.R
 import com.example.assignmentchapter3androidd.fragments.AddMessageFragment
 import com.example.assignmentchapter3androidd.fragments.GalleryFragment
 import com.example.assignmentchapter3androidd.fragments.ViewMessageFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-private fun OnBackPressedDispatcher.addCallback(
-    owner: MainActivity,
-    onBackPressedCallback: Any
-) {
-}
-
-class MainActivity : AppCompatActivity() {
+class FragManActivity : AppCompatActivity() {
 
     private lateinit var mainBottomNav: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_frag_man)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
@@ -39,7 +33,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupBottomNav() {
-        mainBottomNav = findViewById(R.id.main_bottom_nav)
+        mainBottomNav = findViewById(R.id.frag_man_bottom_nav)
 
         mainBottomNav.setOnItemSelectedListener {
             when(it.itemId) {
@@ -54,7 +48,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun inflateFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().apply {
-            replace(R.id.main_fragment_container, fragment)
+            replace(R.id.frag_man_fragment_container, fragment)
         }.commit()
     }
 }
---
+++ Assignment Chapter 3 Android (D)/app/src/main/java/com/example/assignmentchapter3androidd/main/JetNavActivity.kt
@@ -0,0 +1,21 @@
+package com.example.assignmentchapter3androidd.main
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.assignmentchapter3androidd.R
+
+class JetNavActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_jet_nav)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
+    }
+}
